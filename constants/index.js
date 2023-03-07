226a6e707b716b232e5071f15ccb52f2f5f2ed8b
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-03-07 17:57:54 +0530

    frontend

---
+++ constants/index.js
@@ -0,0 +1,67 @@
+export const WHITELIST_CONTRACT_ADDRESS =
+  "0x77F3b052C6Da86a4EbC3279b852901a20583A534";
+export const abi = [
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_maxWhitelistedAddresses",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "addAddressToWhitelist",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "maxWhitelistedAddresses",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "numAddressesWhitelisted",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "whitelistedAddresses",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
